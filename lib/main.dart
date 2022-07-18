b0e1bb6766bc0ec67a4a7b0ff223419310821894
Jonathan Boisvert <jonathanboisvertx99@icloud.com>
2022-07-17 23:14:30 -0400

    added two buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,56 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(Myapp());
 
-class Myapp extends StatelessWidget{
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  static const String _title = ' Flutter buttons';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: _title,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Button Demo"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 8),
-                child: TextButton(
-                  child: Text("TextButton"),
-                  onPressed: (){},
-                )
-              ),
-              Container(
-                margin: EdgeInsets.only(top:8),
-                child: ElevatedButton(
-                  child: Text("ElevatedButton"),
-                  color: Colors.blueAccent,
-                  textColor: Colors.white,
-                  onPressed: (){},
-                ),
-              )
-            ],
-          ),
-        ),
+        body: const MyStatefulWidget(),
       ),
-    )
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('ARM'),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Fire!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
